c03da203ae5dde28fc5252d0b63b311335019945
NabilaAdelina <adelinaputrinabil@gmail.com>
2024-05-18 14:15:06 +0700

    RincianAnggaran

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import LandingPage from "./pages/LandingPage/LandingPage"
 import { Routes, Route } from "react-router-dom"
 import UserSetting from "./pages/SettingProfile/UserSetting"
 import FormReq from "././pages/FormReq/FormReq"
+import RincianAnggaran from "./pages/RincianAnggaran/RincianAnggaran"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/user/profile" element={<UserSetting />}></Route>
         <Route path="/form" element={<FormReq />}></Route>
+        <Route path="/rincian" element={<RincianAnggaran />}></Route>
       </Routes>
     </>
   )
