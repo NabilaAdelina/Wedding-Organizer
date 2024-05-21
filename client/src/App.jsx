6b659ae6ad4700a7b60bc910a40b61131400a5c9
Azriel Nurrahman <azriel9918274572@gmail.com>
2024-05-21 22:39:21 +0700

    add: Schedule

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import FormReq from "./pages/FormReq/FormReq"
 import LandingPage from "./pages/LandingPage/LandingPage"
 import { Routes, Route } from "react-router-dom"
 import SettingProfile from "./pages/SettingProfile/SettingProfile"
+import Schedule from "./pages/Schedule/Schedule"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/profile" element={<SettingProfile />}></Route>
         <Route path="/form" element={<FormReq />}></Route>
+        <Route path="/Schedule" element={<Schedule />}></Route>
       </Routes>
     </>
   )
---
+++ client/src/pages/Schedule/Schedule.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import ScheduleLayout from './ScheduleLayout/Schedule'
+
+const Schedule = () => {
+    return (
+        <>
+            <div>
+            <Header />
+            <ScheduleLayout />
+            <Footer />
+            </div>
+        </>
+    )
+}
+
+export default Schedule
