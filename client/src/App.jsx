fe899ab218d9be3dedb2c3b78b228a24f8908bba
NabilaAdelina <adelinaputrinabil@gmail.com>
2024-05-21 20:59:09 +0700

    Fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import FormReq from "./pages/FormReq/FormReq"
 import LandingPage from "./pages/LandingPage/LandingPage"
 import { Routes, Route } from "react-router-dom"
 import SettingProfile from "./pages/SettingProfile/SettingProfile"
+import AfterFormReq from "././pages/FormReq/AfterFormReq"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/profile" element={<SettingProfile />}></Route>
         <Route path="/form" element={<FormReq />}></Route>
+        <Route path="/Aform" element={<AfterFormReq />}></Route>
       </Routes>
     </>
   )
