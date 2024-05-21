378b26c2422df807a2dda0c9e7e726733e6221e3
Azriel Nurrahman <azriel9918274572@gmail.com>
2024-05-21 23:47:50 +0700

    fix error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import LandingPage from "./pages/LandingPage/LandingPage"
 import { Routes, Route } from "react-router-dom"
 import SettingProfile from "./pages/SettingProfile/SettingProfile"
 import Concept from "./pages/Concept/Concept"
+import Schedule from "./pages/Schedule/Schedule"
+import Profile from "./pages/SettingProfile/SettingLayout/Profile"
+import Setting from "./pages/SettingProfile/SettingLayout/Setting"
+import SignUp from "./pages/Auth/SignUp"
+import Login from "./pages/Auth/Login"
 
 function App() {
 
@@ -10,9 +15,14 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
-        <Route path="/profile" element={<SettingProfile />}></Route>
-        <Route path="/form" element={<FormReq />}></Route>
+        <Route path="aform" element={<AfterFormReq />}></Route>
+        <Route path='user/profile' element={<Profile />} />
+        <Route path='user/setting' element={<Setting />} />
+        <Route path="form" element={<FormReq />}></Route>
+        <Route path="signup" element={<SignUp />}></Route>
+        <Route path="login" element={<Login />}></Route>
         <Route path="/concept" element={<Concept />}></Route>
+        <Route path="/Schedule" element={<Schedule />}></Route>
       </Routes>
     </>
   )
