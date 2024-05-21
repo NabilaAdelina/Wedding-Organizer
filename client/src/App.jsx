67cea2833efcb296b380d244da68dc73e7009c5b
ArifinEko <aeko86869@gmail.com>
2024-05-21 22:16:18 +0700

    add: sigin & signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import LandingPage from "./pages/LandingPage/LandingPage"
 import { Routes, Route } from "react-router-dom"
 import Profile from "./pages/SettingProfile/SettingLayout/Profile"
 import Setting from "./pages/SettingProfile/SettingLayout/Setting"
+import SignUp from "./pages/Auth/SignUp"
+import Login from "./pages/Auth/Login"
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
         <Route path='user/profile' element={<Profile />} />
         <Route path='user/setting' element={<Setting />} />
         <Route path="form" element={<FormReq />}></Route>
+        <Route path="signup" element={<SignUp />}></Route>
+        <Route path="login" element={<Login />}></Route>
       </Routes>
     </>
   )
