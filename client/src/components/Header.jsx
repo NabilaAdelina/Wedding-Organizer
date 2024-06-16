0409a43d7e50ffa32d987ec1cb98330dbbfb38da
NabilaAdelina <adelinaputrinabil@gmail.com>
2024-06-16 20:52:49 +0700

    Fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo3 from '../assets/logo/logo-new.svg'
+import profile from '../assets/icons/profile-icon.svg'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
@@ -31,7 +32,7 @@ const Header = () => {
                     })}
                 </ul>
                 <div className='flex gap-5'>
-                    <Link to={"/user/profile"} className='text-white'>My profile</Link>
+                <img src={profile} alt="" />
                 </div>
             </div>
         </header>
